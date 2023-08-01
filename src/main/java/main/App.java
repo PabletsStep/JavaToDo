9e39d74474f0263f19b4bec7cb50c89bb9d138df
PabletsStep <pablorocha.andrade@gmail.com>
2023-08-01 20:31:45 -0300

    Commit inicial

---
+++ src/main/java/main/App.java
@@ -0,0 +1,50 @@
+package main;
+
+import controller.ProjectController;
+import controller.TaskController;
+import java.util.Date;
+import java.util.List;
+import model.Project;
+import model.Task;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        
+////        ProjectController  projectController = new ProjectController();
+////        
+////        Project project = new Project();
+////        //project.setId(7);
+////        project.setName("Teste3");
+////        project.setDescription("description");
+////        projectController.save(project);
+////        
+////        List<Project> projects = projectController.getAll();
+////        System.out.println("total de projetos = " + projects.size());
+//        
+//        //projectController.removeById(7);
+//        
+//        TaskController taskController = new TaskController();
+//        
+//        Task task = new Task();
+//        task.setIdProject(9);
+////        task.setName("Alterar telas da aplicacao");
+////        task.setDescription("devem ser criadas as telas para cadastros");
+////        task.setNotes("Sem notas");
+////        task.setIsCompleted(false);
+//        task.setDeadline(new Date());
+//        
+//        //taskController.save(task);
+//        
+//        task.setName("Alterar telas da aplicacao");
+//        taskController.update(task);
+        
+//        List<Task> tasks = taskController.getAll(8);
+//        System.out.println("Total de tarefas = " + tasks.size());
+        
+        
+    }
+}
